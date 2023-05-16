13d63712363bcdab4e0a41912b8ed4a57fa68ab6
Shah Sawood <shahsawoodshinwari@gmail.com>
2023-05-16 15:31:17 +0500

    uploading project

---
+++ app.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+from PIL import ImageColor, Image, ImageDraw, ImageFont
+import time
+
+def generate_plain_image(width, height, color):
+    try:
+        rgb_color = ImageColor.getrgb(color)
+        image = Image.new('RGB', (width, height), rgb_color)
+
+        # Add dimensions text to the image
+        draw = ImageDraw.Draw(image)
+        text = f'{width}x{height}'
+        font_path = os.path.join('assets', 'arial.ttf')  # Path to the font file
+        
+        # Adjust the font size based on image size
+        font_size = max(int(min(width, height) / 20), 10)
+        font = ImageFont.truetype(font_path, font_size)
+        
+        text_width, text_height = draw.textsize(text, font)
+        text_position = ((width - text_width) // 2, (height - text_height) // 2)
+        draw.text(text_position, text, fill='black', font=font)
+
+        output_dir = 'output'   # Output directory path
+        os.makedirs(output_dir, exist_ok=True)  # Create output directory if it doesn't exist
+
+        timestamp = int(time.time())
+        filename = f'plain_image_{timestamp}.jpg'  # Unique filename with timestamp
+        filepath = os.path.join(output_dir, filename)  # Output file path
+        image.save(filepath)  # Save the image as JPEG
+        print("Plain image generated successfully!")
+        print("Image filepath:", filename)
+    except ValueError as e:
+        print("Invalid color name:", str(e))
+    except Exception as e:
+        print("Error generating plain image:", str(e))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate a plain color image.')
+    parser.add_argument('--width', type=int, default=300, help='Width of the image')
+    parser.add_argument('--height', type=int, default=300, help='Height of the image')
+    parser.add_argument('--color', default='gray', help='Color of the image')
+    args = parser.parse_args()
+
+    generate_plain_image(args.width, args.height, args.color)
